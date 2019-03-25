066c202917552d10bfdd9ee155052032b2cb9071
AndrewStudenic <36557385+AndrewStudenic@users.noreply.github.com>
2019-03-24 23:07:23 -0700

    pods and workspace

---
+++ Pods/Target Support Files/Pods-VSH_MASTER_APP/Pods-VSH_MASTER_APP-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VSH_MASTER_APP : NSObject
+@end
+@implementation PodsDummy_Pods_VSH_MASTER_APP
+@end
---
+++ Pods/Target Support Files/Pods-VSH_MASTER_APPTests/Pods-VSH_MASTER_APPTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VSH_MASTER_APPTests : NSObject
+@end
+@implementation PodsDummy_Pods_VSH_MASTER_APPTests
+@end
